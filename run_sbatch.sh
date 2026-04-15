eaafb11cd9763470cedba7bcd3f66e709d7a9db5
Eitan Turok <eitan.turok@gmail.com>
2026-04-15 18:40:54 +0300

    x

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -10,4 +10,4 @@ touch "${log_dir}/out.log" "${log_dir}/err.log"
 
 sbatch --partition=long.q --ntasks=1 --cpus-per-task=4 --mem=32G --gres=gpu:quadro_rtx_8000:1 --time=12:00:00 \
   --job-name=${job_name} --output=${log_dir}/out.log --error=${log_dir}/err.log \
-  --wrap=". /usr/local/lmod/lmod/init/bash; module load CUDA/12.2.2; curl -LsSf https://astral.sh/uv/install.sh | sh; source \$HOME/.local/bin/env; source \$HOME/mark_sheinin_lab/code/eitan/.secrets; uv run python src/model.py --speakers '[1,0,0,0]' --batch-size 128 --max-duration 10000ep --loss ce --run-name ce"
+  --wrap=". /usr/local/lmod/lmod/init/bash; module load CUDA/12.2.2; curl -LsSf https://astral.sh/uv/install.sh | sh; source \$HOME/.local/bin/env; source \$HOME/mark_sheinin_lab/code/eitan/.secrets; uv run python src/model.py --speakers '[1,0,0,0]' --batch-size 128 --max-duration 10000ep --loss wce --run-name wce"
