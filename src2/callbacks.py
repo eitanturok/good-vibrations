0c9b95bfbcd5b13596baaea7c1321b33ad141ecb
Eitan Turok <eitan.turok@gmail.com>
2026-05-10 22:11:39 +0300

    log images better

--- src2/callbacks.py
+++ src2/callbacks.py
@@ -1,4 +1,4 @@
-import os, re
+import os
 
 import torch
 import numpy as np
@@ -11,28 +11,35 @@ from composer.loggers import WandBLogger
 
 # ***** MaskVizualizer *****
 
-def _make_input_images(inputs: torch.Tensor, num_images: int):
-    if inputs.shape[0] < num_images:
-        num_images = inputs.shape[0]
-    return inputs[:num_images].unsqueeze(-1).detach().cpu().numpy()
-
 class MaskVisualizer(Callback):
     def __init__(self, num_images, train_interval):
         self.num_images = num_images
         self.train_interval = Time.from_input(train_interval, TimeUnit.EPOCH)
         self.last_train_time_value_logged = -1
         self.last_eval_step_logged = {}
-    def _log_image(self, state: State, logger: Logger, data_name: str, pad_width: int=1):
-        mask_pred, mask_true = state.outputs['mask_pred'], state.batch['mask_true']
-        # add white padding between pred and true masks for easier visualization
-        padding = torch.ones(mask_pred.shape[0], mask_pred.shape[1], pad_width, device=mask_pred.device)
-        image = _make_input_images(torch.cat([mask_pred, padding, mask_true], dim=2), self.num_images)
-        logger.log_images(image, name=data_name, channels_last=True, use_table=False)
+
+    def _log_image(self, state: State, logger: Logger, data_name: str):
+        mask_pred, mask_true, info = state.outputs['mask_pred'], state.batch['mask_true'], state.batch['info']
+        pred_np, true_np = mask_pred.detach().cpu().numpy(), mask_true.detach().cpu().numpy()
+        def _render(i):
+            fig, axes = plt.subplots(ncols=2, figsize=(4, 2))
+            axes[0].imshow(pred_np[i], cmap='gray'); axes[0].set_title('Predicted', fontsize=8); axes[0].axis('off')
+            axes[1].imshow(true_np[i], cmap='gray'); axes[1].set_title('Ground Truth', fontsize=8); axes[1].axis('off')
+            fig.suptitle(f"id={info['sample_id'][i]}\nspks={info['speakers'][i]} objs={info['n_objects'][i]} pos=({info['x_position'][i]},{info['y_position'][i]})", fontsize=6)
+            plt.tight_layout()
+            fig.canvas.draw()
+            arr = np.frombuffer(fig.canvas.buffer_rgba(), dtype=np.uint8).reshape(fig.canvas.get_width_height()[::-1] + (4,))[..., :3]
+            plt.close(fig)
+            return arr
+        imgs = [_render(i) for i in range(len(pred_np))]
+        logger.log_images(imgs, name=data_name, channels_last=True, use_table=False)
+
     def before_loss(self, state: State, logger: Logger):
         current_time_value = state.timestamp.get(self.train_interval.unit).value
         if current_time_value % self.train_interval.value == 0 and current_time_value != self.last_train_time_value_logged:
             self.last_train_time_value_logged = current_time_value
             self._log_image(state, logger, 'Images/train')
+
     def eval_after_forward(self, state: State, logger: Logger):
         eval_batch = state.eval_timestamp.get(TimeUnit.BATCH).value
         train_step = state.timestamp.batch.value
