66b84a337d83bf933c1457568511f31c7ae23b9c
Eitan Turok <eitan.turok@gmail.com>
2026-07-10 17:11:58 +0300

    delete old src, src2 dirs

--- src2/test.py
+++
@@ -1,6 +0,0 @@
-from callbacks import get_forward_outputs
-
-outputs_path = "/Users/eitanturok/good-vibrations/runs/1777531033-pragmatic-bulldog/runs/1777531033-pragmatic-bulldog/forward_outputs"
-rows = get_forward_outputs(outputs_path)
-row = rows[0]
-print(rows)
